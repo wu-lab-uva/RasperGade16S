eaa09802fbe4f1b38d318f444e29e38b6d1daa5c
Yingnan Gao <52718100+gaoyingnan-academic@users.noreply.github.com>
2021-08-25 16:01:50 -0400

    Add files via upload

---
+++ RasperGade16S_utils.R
@@ -0,0 +1,25 @@
+# utility functions to be added
+relative_abundance = function(abundance){
+  return(abundance/sum(abundance))
+}
+#
+sample_GCN_from_error_distribution = function(error,lower.edge="bounce"){
+  this.GCN = round(sapply(error,function(x){
+    rMixNormal(n = 1,mean = x$x,sd = sqrt(x$var),probs = x$probs)
+  }))
+  if(lower.edge=="bounce"){
+    this.GCN[this.GCN<1] = 2-this.GCN[this.GCN<1]
+  }else{
+    this.GCN[this.GCN<1] = 1
+  }
+  return(this.GCN)
+}
+correct_GCN = function(gene.abundance,GCN,normalize =TRUE){
+  cell.abundance = gene.abundance/GCN
+  if(normalize) cell.abundance = relative_abundance(cell.abundance)*sum(gene.abundance)
+  return(cell.abundance)
+}
+#
+correct_abundance_table = function(abundance,GCN,normalize=TRUE){
+  as.data.frame(t(apply(abundance,1,correct_GCN,GCN = GCN,normalize=normalize)))
+}
