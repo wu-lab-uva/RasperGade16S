3e3a5a4556248ad26350a9983d0d10ef1897090e
Yingnan Gao <yg5ap@virginia.edu>
2021-08-31 11:14:39 -0400

    Add reference data

--- R/RasperGade16S_EPA.R
+++ R/RasperGade16S_EPA.R
@@ -2,10 +2,24 @@
 #' @description 
 #' @export
 #' @rdname insert_query_with_EPA
-insert_query_with_EPA = function(seqs,tree,ref,model,save.paths,numCores=1,max.out=1){
+insert_query_with_EPA = function(seqs,tree,ref.seqs,model,save.path,numCores=1,max.out=1){
+  if(missing(save.path)) save.path = "RasperGade16S_EPA/"
+  if(!dir.exists(save.path)) dir.create(path = )
+  if(missing(tree))
+  cmd = sprintf("epa-ng -t %s -s %s -q %s --model %s --preserve-rooting on --outdir %s --redo -T %d --filter-max %d",
+                tree,ref.seqs,seqs,model,save.path,numCores,max.out)
+  cmd.out=system(command = cmd,intern = TRUE)
+  return(list(out=cmd.out,jplace=sprintf("%sepa_result.jplace",save.path)))
+}
+
+#' @title Align and trim query sequence
+#' @description 
+#' @export
+#' @rdname align_and_trim_query
+align_and_trim_query = function(seqs,tree,ref,model,save.paths,numCores=1,max.out=1){
   if(missing(save.path)) stop("Save path has to be provided!")
   cmd = sprintf("epa-ng -t %s -s %s -q %s --model %s --preserve-rooting on --outdir %s --redo -T %d --filter-max %d",
                 tree,ref,seqs,model,save.path,numCores,max.out)
   cmd.out=system(command = cmd,intern = TRUE)
-  return(list(out=cmd.out,jplace=sprintf("%sepa_result.jplace",save.path)))
+  return(list())
 }
---
+++ R/RasperGade16S_data.R
@@ -0,0 +1,25 @@
+#' Default reference tree for RasperGade16S
+#'
+#' A phylo-class object containing 6408 reference tips/genomes
+#'
+#' @format A phylo-class object with 6408 tips, binary and rooted:
+#' \describe{
+#'   \item{tip.label}{reference genome names, in NCBI RefSeq accession number}
+#'   \item{edge.length}{phylogenetic distance between nodes, in substitutions/site}
+#'   \item{edge}{parent node and descendant node of an edge}
+#'   \item{Nnode}{the number of nodes}
+#' }
+#' @source \url{https://github.com/wu-lab-uva/16S-rRNA-GCN-Predcition}
+"RasperGade16S.reftree"
+
+#' Default reference GCN for RasperGade16S
+#'
+#' A named vector of 6408 elements
+#'
+#' @format A named vector with 6408 elements:
+#' \describe{
+#'   \item{values}{16S rRNA GCN of reference genomes}
+#'   \item{names}{reference genome names, in NCBI RefSeq accession number}
+#' }
+#' @source \url{https://github.com/wu-lab-uva/16S-rRNA-GCN-Predcition}
+"RasperGade16S.refGCN"
