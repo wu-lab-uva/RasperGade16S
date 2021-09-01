16ccca6dfd32ed554c704b1acd5463a195e61b04
Yingnan Gao <yg5ap@virginia.edu>
2021-09-01 07:22:54 -0400

    add HMM

--- R/RasperGade16S_EPA.R
+++ R/RasperGade16S_EPA.R
@@ -1,11 +1,11 @@
 #' @title Run EPA from R terminal
-#' @description 
+#' @description Calls EPA-ng and insert query sequences into the reference
 #' @export
 #' @rdname insert_query_with_EPA
 insert_query_with_EPA = function(seqs,tree,ref.seqs,model,save.path,numCores=1,max.out=1,intern=TRUE){
-  if(missing(seqs)) stop("Path to query sequences must be provided!")
+  if(missing(seqs)) stop("Path to trimmed query alignments must be provided!")
   if(missing(save.path)) save.path = "RasperGade16S_EPA/"
-  if(!dir.exists(save.path)) dir.create(path = save.path)
+  if(!dir.exists(save.path)) dir.create(path = save.path,recursive = TRUE)
   if(missing(tree)) tree = system.file("extdata","reference.tre",package = "RasperGade16S",mustWork = TRUE)
   if(missing(ref.seqs)) ref.seqs = system.file("extdata","reference.trimmed.afa",package = "RasperGade16S",mustWork = TRUE)
   if(missing(model)) model = system.file("extdata","reference.RAxML.model.txt",package = "RasperGade16S",mustWork = TRUE)
@@ -16,14 +16,21 @@ insert_query_with_EPA = function(seqs,tree,ref.seqs,model,save.path,numCores=1,m
   return(list(out=cmd.out,jplace=sprintf("%s/epa_result.jplace",save.path)))
 }
 
-#' @title Align and trim query sequence
-#' @description 
+#' @title Align and trim sequence
+#' @description Align use HMMER 3
 #' @export
-#' @rdname align_and_trim_query
-align_and_trim_query = function(seqs,tree,ref,model,save.paths,numCores=1,max.out=1){
-  if(missing(save.path)) stop("Save path has to be provided!")
-  cmd = sprintf("epa-ng -t %s -s %s -q %s --model %s --preserve-rooting on --outdir %s --redo -T %d --filter-max %d",
-                tree,ref,seqs,model,save.path,numCores,max.out)
+#' @rdname align_with_HMM_and_trim
+align_with_HMM_and_trim = function(seqs,hmm,mapali,save.path){
+  if(missing(seqs)) stop("Path to query sequences must be provided!")
+  if(missing(save.path)) save.path = "RasperGade16S_align/"
+  if(!dir.exists(save.path)) dir.create(path = save.path,recursive = TRUE)
+  if(missing(hmm)) hmm = system.file("extdata","16S_core_gg.hmm",package = "RasperGade16S",mustWork = TRUE)
+  if(missing(mapali)) mapali = system.file("extdata","aligned_remove_dot.fasta",package = "RasperGade16S",mustWork = TRUE)
+  if(Sys.info()["sysname"]=="Windows") stop("HMMER3 currently not available on Windows.\nSee https://http://hmmer.org/ for more information.")
+  cmd = sprintf("hmmalign --trim --dna -o %s/seq.align --mapali %s %s %s",
+                save.path,mapali,hmm,seqs)
   cmd.out=system(command = cmd,intern = TRUE)
-  return(list())
+  cmd = sprint("esl-reformat -o %s/seq.afa -u --gapsym=- afa %s/seq.align",save.path,save.path)
+  trim_sequence_with_mask(align = sprintf("%s/seq.afa",save.path),trimmed.align = sprintf("%s/trimmed.afa",save.path))
+  return(list(out=cmd.out,afa = sprintf("%s/trimmed.afa",save.path)))
 }
