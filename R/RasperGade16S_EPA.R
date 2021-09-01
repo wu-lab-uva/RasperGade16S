0f7dbcbd37e7f96890bd5e99dfac2a6bbac392ce
Yingnan Gao <yg5ap@virginia.edu>
2021-09-01 17:08:36 -0400

    Update RasperGade16S_EPA.R

--- R/RasperGade16S_EPA.R
+++ R/RasperGade16S_EPA.R
@@ -10,8 +10,10 @@ insert_query_with_EPA = function(seqs,tree,ref.seqs,model,save.path,numCores=1,m
   if(missing(ref.seqs)) ref.seqs = system.file("extdata","reference.trimmed.afa",package = "RasperGade16S",mustWork = TRUE)
   if(missing(model)) model = system.file("extdata","reference.RAxML.model.txt",package = "RasperGade16S",mustWork = TRUE)
   if(Sys.info()["sysname"]=="Windows") stop("EPA-ng currently not available on Windows.\nSee https://github.com/Pbdas/epa-ng for more information.")
-  cmd = sprintf("epa-ng -t %s -s %s -q %s --model %s --preserve-rooting on --outdir %s --redo -T %d --filter-max %d",
-                tree,ref.seqs,seqs,model,save.path,numCores,max.out)
+  #cmd = sprintf("epa-ng -t %s -s %s -q %s --model %s --preserve-rooting on --outdir %s --redo -T %d --filter-max %d",
+  #              tree,ref.seqs,seqs,model,save.path,numCores,max.out)
+  cmd = sprintf("epa-ng -t %s -s %s -q %s --model %s --preserve-rooting on --outdir %s --redo --filter-max %d",
+                tree,ref.seqs,seqs,model,save.path,max.out)
   print(cmd)
   cmd.out=system(command = cmd,intern = intern)
   return(list(out=cmd.out,jplace=sprintf("%s/epa_result.jplace",save.path)))
